a9ba340529eb68e4a0f5f19223e7628c5b98e1ca
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-24 11:48:14 -0500

    agregar funciona lambda

---
+++ python-desde-cero/proyecto-python/lambdas.py
@@ -0,0 +1,42 @@
+# Funciones lambdas son funciones anonimas que se crean con una sola linea
+# Se pueden usar para crear funciones pequenias y rapidas
+
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    return a / b
+
+suma_lambda = lambda a, b: a + b
+resta_lambda = lambda a, b: a - b
+multiplicar_lambda = lambda a, b: a * b
+dividir_lambda = lambda a, b: a / b
+
+
+def creador_funciones_multiplicar(n):
+    return lambda a: a * n
+
+duplicar = creador_funciones_multiplicar(2)
+triplicar = creador_funciones_multiplicar(3)
+cuadruplicar = creador_funciones_multiplicar(4)
+
+print(duplicar(10))
+print(triplicar(10))
+print(cuadruplicar(10))
+
+# Ejemplo lambda filter
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+pares = list(filter(lambda x: x % 2 == 0, numeros))
+impares = list(filter(lambda x: x % 2 != 0, numeros))
+
+print(pares)
+print(impares)
